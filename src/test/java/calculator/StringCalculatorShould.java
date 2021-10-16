a1e36e0ba3de659a2d6ff583177c8b8d4abb2bb3
PRAVEEN JAINKAR <thepj0705@gmail.com>
2021-10-16 15:54:13 +0530

    Three numbers delimited anyway should be summed.

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -24,15 +24,24 @@ class StringCalculatorShould {
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+        assertEquals(5,StringCalculator.add("5"));
     }
 
     @Test
     public void numbersCommaDelimitedShouldBeSummed() {
         assertEquals(3,stringCalculator.add("1,2"));
+        assertEquals(25,StringCalculator.add("10,15"));
     }
 
     @Test
     public void numbersNewlineDelimitedShouldBeSummed() {
         assertEquals(3,StringCalculator.add("1\n2"));
+        assertEquals(24,stringCalculator.add("11\n13"));
+    }
+
+    @Test
+    public void threeNumbersDelimitedAnywayShouldBeSummed() {
+        assertEquals(6,stringCalculator.add("1,2,3") );
+        assertEquals(10,stringCalculator.add("5\n2\n3"));
     }
 }
